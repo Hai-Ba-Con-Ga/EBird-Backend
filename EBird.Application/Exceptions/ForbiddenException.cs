cb82c57ebbf7b5ae80c5b778f72137c1cd2c91bd
khanhlinh2601 <tklinh2601@gmail.com>
2023-01-16 15:16:16 +0700

    add exception

---
+++ EBird.Application/Exceptions/ForbiddenException.cs
@@ -0,0 +1,31 @@
+﻿using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBird.Application.Exceptions
+{
+    public class ForbiddenException : BaseHttpException
+    {
+        private readonly static int statusCode = StatusCodes.Status403Forbidden;
+
+        public ForbiddenException(object customError) : base(customError, statusCode)
+        {
+        }
+
+        public ForbiddenException(IEnumerable<ValidationError> errors) : base(errors, statusCode)
+        {
+        }
+
+        public ForbiddenException(Exception ex) : base(ex, statusCode)
+        {
+        }
+
+        public ForbiddenException(string message, string errorCode = null, string refLink = null) : base(message, statusCode, errorCode, refLink)
+        {
+        }
+    }
+}
---
+++ EBird.Application/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,30 @@
+﻿using AutoWrapper.Wrappers;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EBird.Application.Exceptions
+{
+    public class UnauthorizedException : BaseHttpException
+    {
+        private readonly static int statusCode = StatusCodes.Status401Unauthorized;
+        public UnauthorizedException(object customError) : base(customError, statusCode)
+        {
+        }
+
+        public UnauthorizedException(IEnumerable<ValidationError> errors) : base(errors, statusCode)
+        {
+        }
+
+        public UnauthorizedException(Exception ex) : base(ex, statusCode)
+        {
+        }
+
+        public UnauthorizedException(string message, string errorCode = null, string refLink = null) : base(message, statusCode, errorCode, refLink)
+        {
+        }
+    }
+}
