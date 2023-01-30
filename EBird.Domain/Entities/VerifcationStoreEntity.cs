da7a00dbb445b42541d58800c28c20a216b138b8
khanhlinh2601 <tklinh2601@gmail.com>
2023-01-30 21:54:10 +0700

    add verfication

--- EBird.Domain/Entities/VerifcationStoreEntity.cs
+++ EBird.Domain/Entities/VerifcationStoreEntity.cs
@@ -1,12 +1,14 @@
 ﻿using EBird.Domain.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EBird.Domain.Entities
 {
+    [Table("VerifcationStore")]
     public class VerifcationStoreEntity : BaseEntity
     {
         public string Code { get; set; }
