540c8f36dff3a7c5975335b5c46ab99e59eef6bb
khanhlinh2601 <tklinh2601@gmail.com>
2023-01-31 22:49:28 +0700

    change response services

--- EBird.Application/Services/AccountServices.cs
+++ EBird.Application/Services/AccountServices.cs
@@ -1,11 +1,11 @@
 ﻿
 using AutoMapper;
+using EBird.Application.Exceptions;
 using EBird.Application.Interfaces;
 using EBird.Application.Interfaces.IRepository;
 using EBird.Application.Model;
 using EBird.Application.Services.IServices;
 using EBird.Domain.Entities;
-using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Manage.Internal;
 using Response;
 using System.Net;
 
@@ -29,79 +29,78 @@ namespace EBird.Application.Services
             _emailServices = emailServices;
         }
 
-        public async Task<Response<AccountResponse>> GetAccountById(Guid id)
+        public async Task<AccountResponse> GetAccountById(Guid id)
         {
             var account = await _accountRepository.GetByIdActiveAsync(id);
 
             if (account == null)
             {
-                return Response<AccountResponse>.Builder().SetStatusCode((int)HttpStatusCode.BadRequest).SetMessage("Account is not exist").SetSuccess(false);
+                throw new NotFoundException("Account is not exist");
             }
             var accountResponse = new AccountResponse();
             _mapper.Map<AccountEntity, AccountResponse>(account, accountResponse);
 
-            return Response<AccountResponse>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetSuccess(true).SetData(accountResponse);
+            return accountResponse;
         }
-        public async Task<Response<List<AccountResponse>>> GetAllAccount()
+        public async Task<List<AccountResponse>> GetAllAccount()
         {
             var accounts = await _accountRepository.GetAllActiveAsync();
             if (accounts == null)
             {
-                return Response<List<AccountResponse>>.Builder().SetStatusCode((int)HttpStatusCode.BadRequest).SetMessage("Account is not exist").SetSuccess(false);
+                throw new NotFoundException("Account is not exist");
             }
-            var accountResponses = new List<AccountResponse>();
+            var accountList = new List<AccountResponse>();
             
             foreach (var account in accounts)
             {
                 var accountResponse = new AccountResponse();
                 _mapper.Map<AccountEntity, AccountResponse>(account, accountResponse);
-                accountResponses.Add(accountResponse);
+                accountList.Add(accountResponse);
             }
-            return Response<List<AccountResponse>>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetSuccess(true).SetData(accountResponses);
+            return accountList;
         }
-        public async Task<Response<string>> UpdateAccount(AccountEntity updateAccount)
+        public async Task UpdateAccount(AccountEntity updateAccount)
         {
             var account = await _accountRepository.GetByIdActiveAsync(updateAccount.Id);
             if (account == null)
             {
-                return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.BadRequest).SetMessage("Account is not exist").SetSuccess(false);
+                throw new NotFoundException("Account is not exist");
             }
             await _accountRepository.UpdateAsync(account);
-            return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetMessage("Update Successful").SetSuccess(true);
 
         }
-        public async Task<Response<string>> DeleteAccount(Guid id)
+        public async Task DeleteAccount(Guid id)
         {
             var account = await _accountRepository.DeleteSoftAsync(id);
             if (account == null)
             {
-                return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.BadRequest).SetMessage("Delete Failed").SetSuccess(false);
+                throw new NotFoundException("Account is not exist");
             }
-            return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetMessage("Delete Successful").SetSuccess(true);
+            
         }
-        public async Task<Response<string>> ChangePassword(Guid id, Model.ChangePasswordModel model)
+        public async Task ChangePassword(Guid id, Model.ChangePasswordModel model)
         {
             var account = await _accountRepository.GetByIdAsync(id);
             if (account == null)
             {
-                throw new Exception("The account is not exist");
+                throw new NotFoundException("The account is not exist");
             }
             var match = _authenticationServices.HashPassword(model.OldPassword).Equals(account.Password);
             if (!match)
             {
-                throw new Exception("The password is incorrect");
+                throw new BadRequestException("The password is incorrect");
             }
 
             account.Password = _authenticationServices.HashPassword(model.NewPassword);
             await _accountRepository.UpdateAsync(account);
-            return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetMessage("Change Password Successful").SetSuccess(true);
+           
         }
-        public async Task<Response<string>> ForgotPassword(string username)
+        public async Task ForgotPassword(string username)
         {
             var account = await _accountRepository.FindWithCondition(p => p.Username.Equals(username));
             if (account == null)
             {
-                throw new Exception("The account is not exist");
+                throw new NotFoundException("The account is not exist");
             }
             Console.WriteLine(account.Id);
             var code = new Random().Next(10000, 99999).ToString();
@@ -121,35 +120,35 @@ namespace EBird.Application.Services
             await _verifcationStoreRepository.CreateAsync(verifcation);
 
             await _emailServices.SendForgotPassword(model);
-            return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetSuccess(true);
+            
         }
-        public async Task<Response<string>> ResetPassword(ResetPasswordModel model)
+        public async Task ResetPassword(ResetPasswordModel model)
         {
             var verification = await _verifcationStoreRepository.FindWithCondition(x => x.AccountId.Equals(model.AccountId) && x.Code.Equals(model.Code));
             if(verification == null)
             {
-                throw new Exception("Code is not exist");
+                throw new NotFoundException("Code is not exist");
             }
             var account = await _accountRepository.GetByIdAsync(model.AccountId);
             if (account == null)
             {
-                throw new Exception("The account is not exist");
+                throw new NotFoundException("The account is not exist");
             }
             account.Password = _authenticationServices.HashPassword(model.Password);
             await _accountRepository.UpdateAsync(account);
             await _verifcationStoreRepository.DeleteAsync(verification.Id);
-            return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetSuccess(true).SetMessage("Reset Password Successful");
+            
 
         }
-        public async Task<Response<string>> CheckEmail(string email)
+        public async Task CheckEmail(string email)
         {
             var account = await _accountRepository.FindWithCondition(x=> x.Email.Equals(email)); 
             if (account == null)
             {
-                return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.BadRequest).SetMessage("Email is not exist").SetSuccess(false);
+                throw new NotFoundException("Email is not exist");
 
             }
-            return Response<string>.Builder().SetStatusCode((int)HttpStatusCode.OK).SetSuccess(true);
+            
 
         }
 
