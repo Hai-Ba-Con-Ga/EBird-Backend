ede5929c4496b2fd6e43a48d32fe787d4fd04b93
Nguyễn Mạnh Hùng <hungnmse160060@fpt.edu.vn>
2023-02-07 17:39:13 +0700

    DB add Ondelete noaction, error vcl

--- EBird.Infrastructure/Context/ApplicationDbContext.cs
+++ EBird.Infrastructure/Context/ApplicationDbContext.cs
@@ -49,11 +49,22 @@ namespace EBird.Infrastructure.Context
                 .HasMany(acc => acc.HandledReports)
                 .WithOne(r => r.HandledBy)
                 .HasForeignKey(r => r.HandledById);
-
             modelBuilder.Entity<AccountEntity>()
                 .HasMany(acc => acc.CreatedReports)
                 .WithOne(r => r.CreatedBy)
                 .HasForeignKey(r => r.CreatedById);
+            //Config for one to many relationship between AccountEntity and FriendshipEntity
+            modelBuilder.Entity<AccountEntity>()
+                .HasMany(acc => acc.InviterFriendships)
+                .WithOne(r => r.Inviter)
+                .HasForeignKey(r => r.InviterId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<AccountEntity>()
+                .HasMany(acc => acc.RecieverFriendships)
+                .WithOne(r => r.Reciever)
+                .HasForeignKey(r => r.RecieverId)
+                .OnDelete(DeleteBehavior.NoAction);
         }
 
         #region DbSet
@@ -61,11 +72,36 @@ namespace EBird.Infrastructure.Context
         public DbSet<RefreshTokenEntity> RefreshTokens { get; set; } = null!;
         public DbSet<VerifcationStoreEntity> VerifcationStores { get; set; } = null!;
 
+        public DbSet<AccountResourceEntity> AccountResources { get; set; }
+
         public DbSet<BirdEntity> Birds { get; set; }
+        public DbSet<BirdResourceEntity> BirdResources { get; set; }
         public DbSet<BirdTypeEntity> BirdTypes { get; set; }
-        public DbSet<RoomEntity> Rooms { get; set; }
+
+        public DbSet<FriendshipEntity> Friendships { get; set; }
+
+        public DbSet<GroupEntity> Groups { get; set; }
+        public DbSet<GroupMemberEntity> GroupMembers { get; set; }
+
+        public DbSet<MatchBirdEntity> MatchBirds { get; set; }
+        public DbSet<MatchEntity> Matchs { get; set; }
+        public DbSet<MatchMessageEntity> MatchMessages { get; set; }
+        public DbSet<MatchResourceEntity> MatchResources { get; set; }
+
+        public DbSet<MessageEntity> Messages { get; set; }
+        public DbSet<MessageGroupEntity> MessageGroups { get; set; }
+        public DbSet<MessagePrivateEntity> MessagePrivates { get; set; }
+
+        public DbSet<NotificationEntity> Notifications { get; set; }
+        public DbSet<NotificationTypeEntity> NotificationTypes { get; set; }
+        
+        public DbSet<PlaceEntity> Places { get; set; }
         public DbSet<PostEntity> Posts { get; set; }
         public DbSet<ReportEntity> Reports { get; set; }
+        public DbSet<RequestEntity> Requests { get; set; }
+        public DbSet<ResourceEntity> Resources { get; set; }
+        public DbSet<RoomEntity> Rooms { get; set; }
+        public DbSet<RuleEntity> Rules { get; set; }
 
         #endregion
     }
