36e3547ebcc5903f9dabcc6a69f8867708d5bdf3
Nguyễn Mạnh Hùng <hungnmse160060@fpt.edu.vn>
2023-02-07 09:01:23 +0700

    [BE] Create DB - PlaceEntity, RequestEntity

---
+++ EBird.Domain/Entities/PlaceEntity.cs
@@ -0,0 +1,39 @@
+﻿using EBird.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EBird.Domain.Entities
+{
+    [Table("Place")]
+    public class PlaceEntity : BaseEntity
+    {
+        [Required]
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(100)]
+        public string Address { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime CreateDatetime { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(100)]
+        public string? Longitude { get; set; }
+
+        [Column(TypeName = "nvarchar")]
+        [MaxLength(100)]
+        public string? Latitude { get; set; }
+
+        // relationship
+        public ICollection<RequestEntity> Requests { get; set; }
+        public ICollection<MatchEntity> Matchs { get; set; }
+
+
+    }
+}
---
+++ EBird.Domain/Entities/RequestEntity.cs
@@ -0,0 +1,45 @@
+﻿using EBird.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EBird.Domain.Entities
+{
+    [Table("Request")]
+    public class RequestEntity : BaseEntity
+    {
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime CreateDatetime { get; set; }
+
+        [Required]
+        [Column(TypeName = "datetime")]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar")]
+        [MaxLength(50)]
+        public string Status { get; set; } = null!;
+
+        // Pk
+        [ForeignKey("CreatedById")]
+        public Guid CreatedById { get; set; }
+        public AccountEntity CreatedBy { get; set; } = null!;
+
+        [ForeignKey("PlaceId")]
+        public Guid PlaceId { get; set; }
+        public PlaceEntity Place { get; set; } = null!;
+        
+        [ForeignKey("RoomId")]
+        public Guid RoomId { get; set; }
+        public RoomEntity Room { get; set; } = null!;
+
+        [ForeignKey("BirdId")]
+        public Guid BirdId { get; set; }
+        public BirdEntity Bird { get; set; } = null!;
+
+        [ForeignKey("GroupId")]
+        public Guid GroupId { get; set; }
+        public GroupEntity? Group { get; set; }
+    }
+}
