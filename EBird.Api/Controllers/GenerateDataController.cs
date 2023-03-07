2b87f3b782ee5a733f29893587b3acdfa4b42e5c
xWyvernPx <phonglethanh2@gmail.com>
2023-03-07 23:42:21 +0700

    [Data] update

--- EBird.Api/Controllers/GenerateDataController.cs
+++ EBird.Api/Controllers/GenerateDataController.cs
@@ -8,17 +8,17 @@ using Response;
 
 namespace EBird.Api.Controllers
 {
-    
+
     [ApiController]
     [Route("generate")]
     public class GenerateDataController : ControllerBase
     {
- private readonly IWebHostEnvironment _env;
+        private readonly IWebHostEnvironment _env;
 
-    public GenerateDataController(IWebHostEnvironment env)
-    {
-        _env = env;
-    }
+        public GenerateDataController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
         [HttpPost("room")]
         public ActionResult<Response<string>> GenerateRoomData()
         {
@@ -38,39 +38,38 @@ namespace EBird.Api.Controllers
         {
             try
             {
-        //         private  readonly ProcessStartInfo cmdStartInfo = new()
-        // {
-        //     FileName = "cmd.exe",
-        //     RedirectStandardInput = true,
-        //     RedirectStandardOutput = true,
-        //     UseShellExecute = false
-        // };
+                //         private  readonly ProcessStartInfo cmdStartInfo = new()
+                // {
+                //     FileName = "cmd.exe",
+                //     RedirectStandardInput = true,
+                //     RedirectStandardOutput = true,
+                //     UseShellExecute = false
+                // };
 
-        // private  readonly string[] commands =
-        // {
-        //     @"cd C:\Path\To\Sql\Scripts", // Change to the directory containing the SQL script
-        //     @"sqlcmd -S YourServerName -d YourDatabaseName -U YourUsername -P YourPassword -i script.sql" // Execute the SQL script
-        // };
-        System.Console.WriteLine($"{_env.ContentRootPath}data/script/account_gen.py");
+                string[] commands = {
+                    @"cd C:\Path\To\Sql\Scripts", // Change to the directory containing the SQL script
+                    @"sqlcmd -S wyvernp.database.windows.net -d globird -U server_admin -P WyvernP2506 -i ./account.sql" };
+                
+                System.Console.WriteLine($"{_env.ContentRootPath}data/script/account_gen.py");
                 //action to do  here
                 string pythonScriptPath = $"{_env.ContentRootPath}data/script/account_gen.py";
                 string pythonExecutablePath = "python3";
 
-        var process = new Process
-        {
-            StartInfo = new ProcessStartInfo
-            {
-                FileName = pythonExecutablePath,
-                Arguments = pythonScriptPath,
-                RedirectStandardOutput = true,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            }
-        };
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = pythonExecutablePath,
+                        Arguments = pythonScriptPath,
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
 
-        process.Start();
-        string result = process.StandardOutput.ReadToEnd();
-        process.WaitForExit();
+                process.Start();
+                string result = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
                 return Ok();
             }
             catch (Exception ex)
@@ -80,11 +79,12 @@ namespace EBird.Api.Controllers
         }
 
         [HttpPost("bird")]
-        public ActionResult<Response<string>> GenerateBirdData()
+        public ActionResult<Response<string>> GenerateBirdData([FromBody] BirdGenDTO body)
         {
             try
             {
                 //action to do  here
+                System.Console.WriteLine(body.UserId);
                 return Ok();
             }
             catch (Exception ex)
@@ -134,5 +134,10 @@ namespace EBird.Api.Controllers
                 return BadRequest(new Response<string> { Data = ex.Message });
             }
         }
+    }
+    public class BirdGenDTO
+    {
+        public string UserId { get; set; }
+        public int NumberRecords { get; set; }
     }
 }
