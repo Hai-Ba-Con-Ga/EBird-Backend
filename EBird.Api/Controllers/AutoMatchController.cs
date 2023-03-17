2e920b78e49b119f889076d17c267821c92c7ede
khanhlinh2601 <tklinh2601@gmail.com>
2023-03-17 21:52:10 +0700

    fix conflict

--- EBird.Api/Controllers/AutoMatchController.cs
+++ EBird.Api/Controllers/AutoMatchController.cs
@@ -20,37 +20,32 @@ namespace EBird.Api.Controllers;
 [ApiController]
 public class AutoMatchController : ControllerBase
 {
-  private readonly IRequestService _requestService;
-  private readonly IMatchingService _matchingService;
-  private readonly IMapper _mapper;
+    private readonly IRequestService _requestService;
+    private readonly IMatchingService _matchingService;
+    private readonly IMapper _mapper;
 
-  public AutoMatchController(IRequestService requestService, IMatchingService mathcingService, IMapper mapper)
-  {
-    _requestService = requestService;
-    _matchingService = mathcingService;
-    _mapper = mapper;
-  }
-
-  [HttpGet("group/{groupid}")]
-  public async Task<ActionResult<Response<List<Tuple<Guid, Guid>>>>> AutoMatchGroup(Guid groupid)
-  {
-    var response = new Response<List<Tuple<Guid, Guid>>>();
-    try
+    public AutoMatchController(IRequestService requestService, IMatchingService mathcingService, IMapper mapper)
     {
-      ////Test
-      //dynamic jsonarray = _matchingService.LoadJson("request_tuple_mock_data.json");
-      //List<RequestTuple> lq = jsonarray.ToObject<List<RequestTuple>>();
-      //var test = await _matchingService.BinarySearch(lq);
-      //for (int i = 0; i < test.Count; i++)
-      //{
-      //    System.Console.WriteLine($"{test[i].Item1} + {test[i].Item2}");
-      //}
-      var parameters = new RequestParameters();
-      var list = (await _requestService.GetRequestsByGroupId(groupid, parameters))
-          //var list = (await _requestService.GetRequests())
-          .Where(x => x.Status.Equals(RequestStatus.Waiting)).ToList();
-      var finder = new RequestTuple();
-      var listRequest = new List<RequestTuple>();
+        _requestService = requestService;
+        _matchingService = mathcingService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("group/{groupid}")]
+    public async Task<ActionResult<Response<List<Tuple<Guid, Guid>>>>> AutoMatchGroup(Guid groupid)
+    {
+        var response = new Response<List<Tuple<Guid, Guid>>>();
+        try
+        {
+            ////Test
+            //dynamic jsonarray = _matchingService.LoadJson("request_tuple_mock_data.json");
+            //List<RequestTuple> lq = jsonarray.ToObject<List<RequestTuple>>();
+            //var test = await _matchingService.BinarySearch(lq);
+            //for (int i = 0; i < test.Count; i++)
+            //{
+            //    System.Console.WriteLine($"{test[i].Item1} + {test[i].Item2}");
+            //}
+
 
             RequestParameters parameters = new RequestParameters();
 
@@ -60,12 +55,12 @@ public class AutoMatchController : ControllerBase
             var finder = new RequestTuple();
             var listRequest = new List<RequestTuple>();
 
-            foreach(var item in list)
+            foreach (var item in list)
             {
                 var requestTuple = new RequestTuple(
                     item.Id,
                     item.Host.Id,
-                    ((double) item.Place.Latitude, (double) item.Place.Longitude),
+                    ((double)item.Place.Latitude, (double)item.Place.Longitude),
                     item.HostBird.Elo,
                     item.RequestDatetime,
                     false
@@ -82,17 +77,17 @@ public class AutoMatchController : ControllerBase
 
             response = Response<List<Tuple<Guid, Guid>>>.Builder()
                         .SetSuccess(true)
-                        .SetStatusCode((int) HttpStatusCode.OK)
+                        .SetStatusCode((int)HttpStatusCode.OK)
                         .SetMessage("Requests are retrieved successfully")
                         .SetData(priorityRequestList);
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
-            if(ex is NotFoundException || ex is BadRequestException)
+            if (ex is NotFoundException || ex is BadRequestException)
             {
                 response = Response<List<Tuple<Guid, Guid>>>.Builder()
                         .SetSuccess(false)
-                        .SetStatusCode((int) HttpStatusCode.BadRequest)
+                        .SetStatusCode((int)HttpStatusCode.BadRequest)
                         .SetMessage(ex.Message);
 
                 return response;
@@ -100,11 +95,9 @@ public class AutoMatchController : ControllerBase
 
             response = Response<List<Tuple<Guid, Guid>>>.Builder()
                         .SetSuccess(false)
-                        .SetStatusCode((int) HttpStatusCode.InternalServerError)
+                        .SetStatusCode((int)HttpStatusCode.InternalServerError)
                         .SetMessage(ex.Message);
         }
         return response;
     }
-    return response;
-  }
 }
