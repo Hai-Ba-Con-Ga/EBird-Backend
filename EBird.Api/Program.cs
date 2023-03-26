5bd920b9eb85fb197a294b18e6af4d22ef87eed5
xWyvernPx <phonglethanh2@gmail.com>
2023-03-26 12:45:08 +0700

    fix wss

--- EBird.Api/Program.cs
+++ EBird.Api/Program.cs
@@ -115,7 +115,9 @@ app.UseAuthorization();
 app.UseAuthentication();
 
 app.MapControllers();
-app.MapHub<ChatHub>("/hub/chat");
+app.MapHub<ChatHub>("/hub/chat",option => {
+  option.Transports = Microsoft.AspNetCore.Http.Connections.HttpTransportType.WebSockets;
+});
 app.MapHub<TestHub>("/hub/test");
 // app.MapHub<RequestHub>("/requestHub");
 
